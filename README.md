eb6fc3b84e49b0e8915eda651349985b191d78b1
Dorus Rolvink <dwrolvink@gmail.com>
2021-12-31 23:34:18 +0100

    update  code and readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# arduino-pro-micro
+# arduino-pro-micro keyboard setup
+## Introduction
+In this repository there is some code for my custom keyboard. 
+
+## Setup
+I use an arduino pro micro, but I have to select Arduino Leonardo in the Arduino IDE. On Linux I also have to start the IDE as sudo in order to be able to connect to the device on the port. (There are
+better ways, but I am lazy).
+
+Then I have to 
+- Go to Tools>Manage Libraries and install Keyboard for the code to work. 
+- Go to Tools>Board>Board Manager and install Arduino AVR Boards to be able to select Arduino Leonardo.
+- A restart of the IDE, then select board Arduino Leonardo, and Tools>Port>(...) (Leonardo)
+
+## Wiring of the switches
+### Basics - one key
+See [reference](https://www.arduino.cc/en/Tutorial/BuiltInExamples/KeyboardMessage) for how to wire the keyswitch up.
+
+In short: connect one end of the switch with 5v (VCC), and the other end to the input pin. The end of the switch that is connected to the input pin should also be connected to a 10K resistor that is 
+in turn connected to ground. This will cause the input pin to be reliably low when not pressed, and high when pressed.
+
+> When using a matrix, one such pulldown resistor is needed per input row/column. 
+
+### Diode Matrix 
+> I am currently waiting for my diodes to arrive, I will continue this write up when that happens.
+
+![diode_matrix](docs/diode_matrix.png)
--- arduino_keyboard_hid.ino
+++ arduino_keyboard_hid.ino
@@ -1,3 +1,4 @@
+
 // Supported for Leonardo, Esplora, Zero, Due and MKR Family (incl Pro Micro)
 
 // This is a "built-in" library no need to install on Windows
@@ -8,17 +9,17 @@
 // Set which pins correspond to which keys
 // Be sure to route switches between pin and ground, as we do a pullup later on
 // this doubles as the id in "previously_pressed"
-#define KEY1   9
-#define KEY2   8
-#define KEY3   7
-#define KEY4   6
-#define KEY5   5
-#define KEY6   4
-#define KEY7   3
-#define KEY8   2
+// #define KEY1   9
+// #define KEY2   8
+// #define KEY3   7
+// #define KEY4   6
+// #define KEY5   5
+// #define KEY6   4
+// #define KEY7   3
+// #define KEY8   2
 
 // list of all keys for easy iteration
-int keys[] = { KEY1, KEY2, KEY3, KEY4, KEY5, KEY6, KEY7, KEY8 };
+//int keys[] = { KEY1, KEY2, KEY3, KEY4, KEY5, KEY6, KEY7, KEY8 };
 
 // keep track of which keys were pressed last cycle
 bool previously_pressed[10];
@@ -35,15 +36,8 @@ void setup()
    *  Default output of any pin will be 1, when the key is pressed, the pin registers
    *  0. This is to get rid of noise, mostly.
    */ 
-   
-  pinMode(KEY1,INPUT_PULLUP);  // sets pin 2 to input & pulls it high w/ internal resistor
-  pinMode(KEY2,INPUT_PULLUP);  // sets pin 3 to input & pulls it high w/ internal resistor
-  pinMode(KEY3,INPUT_PULLUP);  // sets pin 4 to input & pulls it high w/ internal resistor
-  pinMode(KEY4,INPUT_PULLUP);  // sets pin 5 to input & pulls it high w/ internal resistor
-  pinMode(KEY5,INPUT_PULLUP);  // sets pin 6 to input & pulls it high w/ internal resistor
-  pinMode(KEY6,INPUT_PULLUP);  // sets pin 7 to input & pulls it high w/ internal resistor
-  pinMode(KEY7,INPUT_PULLUP);  // sets pin 8 to input & pulls it high w/ internal resistor
-  pinMode(KEY8,INPUT_PULLUP);  // sets pin 9 to input & pulls it high w/ internal resistor
+
+  pinMode(8,INPUT);  // sets pin 8 to input & pulls it high w/ internal resistor
   
   Serial.begin(9600);          // begin serial comms for debugging
   Keyboard.begin();            // begin keyboard 
@@ -56,9 +50,9 @@ void setup()
 void loop() 
 {
    // check each defined key, and press/release virtual key if warranted
-   for (int i = 0; i <= sizeof(keys); i++) {  
-      CheckKey(keys[i]);
-   }
+   //for (int i = 0; i <= sizeof(keys); i++) {  
+      CheckKey(8);
+   //}
     
    delay(1);
 }
@@ -66,7 +60,7 @@ void loop()
 void CheckKey(int id)
 {
   // check if the physical key is being pressed at the moment
-  bool is_pressed_now = (digitalRead(id) == 0);
+  bool is_pressed_now = (digitalRead(id) == 1);
   
   // check if the physical key has been pressed during the last check
   bool active = previously_pressed[id];
@@ -88,31 +82,33 @@ void CheckKey(int id)
 // See also https://www.arduino.cc/reference/en/language/functions/usb/keyboard/keyboardmodifiers/
 void PressKey(int id)
 {
+  
   switch (id) {
-    case KEY1:    
-      Keyboard.press('a');
-      break;
-    case KEY2:    
+    // case KEY1:    
+    //   Keyboard.press('a');
+    //   break;
+    case KEY2:
+      Serial.println("press");
       Keyboard.press('b');
       break;
-    case KEY3:    
-      Keyboard.press('c');
-      break;
-    case KEY4:    
-      Keyboard.press('d');
-      break;
-    case KEY5:    
-      Keyboard.press('e');
-      break;
-    case KEY6:    
-      Keyboard.press('f');
-      break;
-    case KEY7:    
-      Keyboard.press('g');
-      break;
-    case KEY8:    
-      Keyboard.press('h');
-      break;                       
+    // case KEY3:    
+    //   Keyboard.press('c');
+    //   break;
+    // case KEY4:    
+    //   Keyboard.press('d');
+    //   break;
+    // case KEY5:    
+    //   Keyboard.press('e');
+    //   break;
+    // case KEY6:    
+    //   Keyboard.press('f');
+    //   break;
+    // case KEY7:    
+    //   Keyboard.press('g');
+    //   break;
+    // case KEY8:    
+    //   Keyboard.press('h');
+    //   break;                       
   }  
 }
 
@@ -121,29 +117,30 @@ void PressKey(int id)
 void ReleaseKey(int id)
 {
   switch (id) {
-    case KEY1:    
-      Keyboard.release('a');
-      break;
-    case KEY2:    
+    // case KEY1:    
+    //   Keyboard.release('a');
+    //   break;
+    case KEY2:
+      Serial.println("release");
       Keyboard.release('b');
       break;
-    case KEY3:    
-      Keyboard.release('c');
-      break;
-    case KEY4:    
-      Keyboard.release('d');
-      break;
-    case KEY5:    
-      Keyboard.release('e');
-      break;
-    case KEY6:    
-      Keyboard.release('f');
-      break;
-    case KEY7:    
-      Keyboard.release('g');
-      break;
-    case KEY8:    
-      Keyboard.release('h');
-      break;                       
+    // case KEY3:    
+    //   Keyboard.release('c');
+    //   break;
+    // case KEY4:    
+    //   Keyboard.release('d');
+    //   break;
+    // case KEY5:    
+    //   Keyboard.release('e');
+    //   break;
+    // case KEY6:    
+    //   Keyboard.release('f');
+    //   break;
+    // case KEY7:    
+    //   Keyboard.release('g');
+    //   break;
+    // case KEY8:    
+    //   Keyboard.release('h');
+    //   break;                       
   }  
 }
